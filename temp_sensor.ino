66ee5ea641daa7efe9c3cbd5f97b16de25d8ed8a
yamini saraswathi <103330658+yaminiakkala@users.noreply.github.com>
2022-04-29 11:59:36 +0530

    Add files via upload

---
+++ temp_sensor.ino
@@ -0,0 +1,28 @@
+int val;
+int tempPin=A1;
+
+void setup() {
+  Serial.begin(9600);
+ 
+
+}
+
+void loop() {
+  val=analogRead(tempPin);
+  float mv=(val/1024.0)*5000;
+  float cel=mv/10;
+  float farh=(cel*9)/5+32;
+  //Temperature in Celcius
+  Serial.print("TEMPERATURE=");
+  Serial.print(cel);
+  Serial.print("*c");
+  Serial.println();
+  //Temperature in fahrenheit
+  Serial.print("TEMPERATURE=");
+  Serial.print(farh);
+  Serial.print("*F");
+  Serial.println();
+  
+  
+ 
+}
